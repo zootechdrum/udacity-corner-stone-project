dba63ec4d66e0c9059dd5bcba0ca72ba5a894d8a
Cesar Gomez <zootechdrum@gmail.com>
2021-04-02 22:33:33 -0700

    backend server is up and running

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,3 +3,26 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow_Headers',
+            'Content-Type,Authorization, true')
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    @app.route('/', methods=['GET'])
+    def home():
+        return jsonify({
+            'success': 'Hello:World'
+        })
+
+    return app
