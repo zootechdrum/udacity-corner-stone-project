eed0b2deff7588b29eb205c5e8c397fe5f4d7530
Cesar Gomez <zootechdrum@gmail.com>
2021-04-05 22:00:29 -0700

    fixed error for postgres connection

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db
 
 
 def create_app(test_config=None):
@@ -54,5 +57,40 @@ def create_app(test_config=None):
         return jsonify({
             'success': 'Hello:World'
         })
+
+    
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Resource Not Found'
+        }), 404
+
+    @app.errorhandler(422)
+    def unable_to_process(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Could not processs'
+        }), 422
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            'success': False,
+            'error': 405,
+            'message': 'Method not allowed'
+        }), 405
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request'
+        }), 400
+
 
     return app
--- backend/models.py
+++ backend/models.py
@@ -2,12 +2,31 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, DateTime
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 database_name = os.environ.get('database_name')
 database_port = os.environ.get('database_port')
 print(database_name)
 
-database_path = 'postgres://{}/{}',format(database_port,database_name)
+database_path = 'postgres://{}/{}'.format(database_port,database_name)
+
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    __tablename__='movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+
+class Actor(db.Model):
+    __tablename__='actors'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
